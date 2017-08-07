f34f6a12386f84aeae51f032d63f0e5413b3666e
Daniel Sikar <dsikar@tycoint.com>
2017-08-07 12:00:39 +0100

    Adding another operation.

--- tensorflow/tf-tutorial.py
+++ tensorflow/tf-tutorial.py
@@ -15,3 +15,7 @@ b = tf.placeholder(tf.float32)
 adder_node = a + b  # + provides a shortcut for tf.add(a, b)
 print(sess.run(adder_node, {a: 3, b:4.5}))
 print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
+
+# Adding another operation
+add_and_triple = adder_node * 3.
+print(sess.run(add_and_triple, {a: 3, b:4.5}))
