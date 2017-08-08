706b9268160d896cdfa3dea89c3c4a0bc63d02ff
dsikar <dsikar@gmail.com>
2017-08-08 11:27:10 +0100

    Added training.

--- tensorflow/tf-getting-started.py
+++ tensorflow/tf-getting-started.py
@@ -39,9 +39,17 @@ squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
 print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
 
-
 # Reassigning
 fixW = tf.assign(W, [-1.])
 fixb = tf.assign(b, [1.])
 sess.run([fixW, fixb])
 print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+# Training
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+sess.run(init) # reset values to incorrect defaults.
+for i in range(1000):
+      sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
+
+      print(sess.run([W, b]))
