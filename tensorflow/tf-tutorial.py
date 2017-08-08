e81d087c756f819d0b5f8122d5f017786a434186
dsikar <dsikar@gmail.com>
2017-08-08 10:17:27 +0100

    Reassignment

--- tensorflow/tf-tutorial.py
+++ tensorflow/tf-tutorial.py
@@ -38,3 +38,10 @@ y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
 print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+
+# Reassigning
+fixW = tf.assign(W, [-1.])
+fixb = tf.assign(b, [1.])
+sess.run([fixW, fixb])
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
