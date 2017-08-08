16529dfd69e18534330465812ee6b3c23a3002cc
Daniel Sikar <dsikar@tycoint.com>
2017-08-08 09:59:26 +0100

    Variables

--- tensorflow/tf-tutorial.py
+++ tensorflow/tf-tutorial.py
@@ -19,3 +19,16 @@ print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
 # Adding another operation
 add_and_triple = adder_node * 3.
 print(sess.run(add_and_triple, {a: 3, b:4.5}))
+
+# Variables
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+# Initialise
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# Evaluate
+print(sess.run(linear_model, {x:[1,2,3,4]}))
