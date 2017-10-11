99d248dccf7f9cc2014b253f6217191014a76bc5
Daniel Sikar <dsikar@gmail.com>
2017-10-11 09:57:48 +0100

    Create zero-w-setup.sh

---
+++ raspberrypi/zero-w-setup.sh
@@ -0,0 +1,18 @@
+# Setting up the Raspberry Pi Zero W v1.1
+
+# SD Card preparation
+# 1. SD Card - Sandisk 16GB Ultra Micro SD SDHC Memory Card 48MB/s UHS-I Class 10
+# http://www.ebay.co.uk/itm/202013230777
+# 2. Image - "RASPBIAN STRETCH WITH DESKTOP" https://downloads.raspberrypi.org/raspbian_latest
+# 3. Imager - Win32 Disk Imager https://sourceforge.net/projects/win32diskimager/
+
+# Internet connection setup via desktop
+
+# Update & upgrade
+sudo apt-get update && apt-get upgrade
+
+# Install apache2
+sudo apt-get install apache2 
+
+# Enable ssh
+# TODO
