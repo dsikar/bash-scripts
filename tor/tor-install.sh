46c6115b13e3fc6126ecd86213dda790db909ca6
Daniel Sikar <dsikar@gmail.com>
2017-10-24 14:25:23 +0000

    32 bit version for raspberry pi.

--- tor/tor-install.sh
+++ tor/tor-install.sh
@@ -15,15 +15,15 @@ wget https://www.ovpn.com/download/configurations/tor-ovpn.zip
 # unpack
 unzip tor-ovpn.zip
 # save as config file
-mv tor-ovpn.ovpn .torrc
+mv tor-ovpn.ovpn ~/.torrc
 # clean up
 rm tor-ovpn.zip
 # get browser - 32 bit version (in this case)
-# wget https://www.torproject.org/dist/torbrowser/7.5a1/tor-browser-linux64-7.5a1_en-US.tar.xz
-wget https://dist.torproject.org/torbrowser/7.5a6/tor-browser-linux64-7.5a6_en-US.tar.xz
-tar -xvf tor-browser-linux64-7.5a6_en-US.tar.xz
+# wget https://www.torproject.org/dist/torbrowser/7.5a1/tor-browser-linux32-7.5a1_en-US.tar.xz
+wget https://dist.torproject.org/torbrowser/7.5a6/tor-browser-linux32-7.5a6_en-US.tar.xz
+tar -xvf tor-browser-linux32-7.5a6_en-US.tar.xz
 # clean up
-rm tor-browser-linux64-7.5a6_en-US.tar.xz
+rm tor-browser-linux32-7.5a6_en-US.tar.xz
 # launch browser (will launch firefor)
 cd tor-browser_en-US/
 ./start-tor-browser.desktop
