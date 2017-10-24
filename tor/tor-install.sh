f74e89878ace5cb45deac5b3438cb0a0454cdf35
Daniel Sikar <dsikar@gmail.com>
2017-10-24 13:26:30 +0000

    Updated tor repository url

--- tor/tor-install.sh
+++ tor/tor-install.sh
@@ -7,9 +7,9 @@ sudo apt-get update
 # move to home dir
 cd ~/
 # install vpn
-sudo apt install openvpn easy-rsa
+sudo apt install openvpn easy-rsa -y
 # install tor
-sudo apt-get install tor
+sudo apt-get install tor -y
 # get tor config file
 wget https://www.ovpn.com/download/configurations/tor-ovpn.zip
 # unpack
@@ -19,10 +19,11 @@ mv tor-ovpn.ovpn .torrc
 # clean up
 rm tor-ovpn.zip
 # get browser - 32 bit version (in this case)
-wget https://www.torproject.org/dist/torbrowser/7.5a1/tor-browser-linux64-7.5a1_en-US.tar.xz
-tar -xvf tor-browser-linux64-7.5a1_en-US.tar.xz
+# wget https://www.torproject.org/dist/torbrowser/7.5a1/tor-browser-linux64-7.5a1_en-US.tar.xz
+wget https://dist.torproject.org/torbrowser/7.5a6/tor-browser-linux64-7.5a6_en-US.tar.xz
+tar -xvf tor-browser-linux64-7.5a6_en-US.tar.xz
 # clean up
-rm tor-browser-linux64-7.5a1_en-US.tar.xz
+rm tor-browser-linux64-7.5a6_en-US.tar.xz
 # launch browser (will launch firefor)
 cd tor-browser_en-US/
 ./start-tor-browser.desktop
