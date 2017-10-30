87e634478a8aff8f1664ce3e9dc2b63b3446e072
Daniel Sikar <dsikar@gmail.com>
2017-10-30 09:29:54 +0000

    Faster raspistill snaps.

--- raspberrypi/upload.sh
+++ raspberrypi/upload.sh
@@ -2,7 +2,7 @@
 
 while [ true ]
 do
-  sudo raspistill -o image.jpg -w 640 -h 480 -rot 180 -ifx sketch
+  sudo raspistill -q 5 --nopreview --timeout 1 -o image.jpg -w 640 -h 480 -rot 180 -ifx sketch
   scp -i ~/.ssh/pikey image.jpg  mailinfo@sikarsystems.com:~/public_html/images
   sleep 1
 done 
