2a17f221738cc80912e18b2b052dccd9e1f348ee
Daniel Sikar <dsikar@gmail.com>
2017-10-31 13:13:06 +0000

    Using sftp.

--- raspberrypi/upload.sh
+++ raspberrypi/upload.sh
@@ -3,6 +3,6 @@
 while [ true ]
 do
   sudo raspistill -q 5 --nopreview --timeout 1 -o image.jpg -w 640 -h 480 -rot 180 -ifx sketch
-  scp -i ~/.ssh/pikey image.jpg  mailinfo@sikarsystems.com:~/public_html/images
-  sleep 1
+  sftp -i ~/.ssh/pikey mailinfo@sikarsystems.com:public_html/images/ <<< $'put image.jpg'
+  # sleep 1
 done 
