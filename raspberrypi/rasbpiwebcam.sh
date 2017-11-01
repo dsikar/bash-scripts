b3a72df9753e4dadfaed2e086cbf6e44b1c5b011
Daniel Sikar <dsikar@gmail.com>
2017-11-01 09:50:29 +0000

    Tweaked loop to stop camera from locking when script is stopped.

--- raspberrypi/rasbpiwebcam.sh
+++ raspberrypi/rasbpiwebcam.sh
@@ -9,9 +9,11 @@ if [ ! -f $STARTFILE ]; then
   echo "To stop webcam delete $STARTFILE"
 fi
 
-while [ -f $STARTFILE ]
+while [ true ]
 do
-  sudo raspistill -q 3 --nopreview --timeout 1 -o image.jpg -w 640 -h 480 -rot 180 -ifx sketch
-  sftp -i ~/.ssh/pikey mailinfo@sikarsystems.com:public_html/images/ <<< $'put image.jpg'
-  # sleep 1
+  if [ -f $STARTFILE ]; then
+    sudo raspistill -q 3 --nopreview --timeout 1 -o image.jpg -w 640 -h 480 -rot 180 -ifx sketch 
+    sftp -i ~/.ssh/pikey mailinfo@sikarsystems.com:public_html/images/ <<< $'put image.jpg' 
+    sleep 5
+  fi
 done 
--- raspberrypi/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while [ true ]
-do
-  sudo raspistill -q 5 --nopreview --timeout 1 -o image.jpg -w 640 -h 480 -rot 180 -ifx sketch
-  sftp -i ~/.ssh/pikey mailinfo@sikarsystems.com:public_html/images/ <<< $'put image.jpg'
-  # sleep 1
-done 
