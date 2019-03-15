3e53406336f99feea0cad4378c259c7754e83efc
dsikar <dsikar@gmail.com>
2019-03-15 23:19:26 +0000

    Added week 08 cloud exercise

--- gcloud/run-gcloud-job.sh
+++ gcloud/run-gcloud-job.sh
@@ -39,3 +39,16 @@ gsutil rb gs://${PROJECT_ID}
 
 # remove local data
 rm -rf Data-Science-City
+
+# week 08
+# make bucket 
+gsutil mb -c regional -l europe-west1 -p inm432 gs://inm432-week08
+
+# download data
+wget http://files.grouplens.org/datasets/movielens/ml-20m.zip
+
+# decompress
+unzip ml-20m.zip
+
+# store in bucket - charges apply
+gsutil cp -r ml-20m gs://inm432-week08/
